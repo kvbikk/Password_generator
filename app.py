9aa728931b35f8dd0e6d4984ef1ea0e8e4152bec
Kacper Kubik <kacper-kubik@o2.pl>
2026-03-20 01:08:51 +0100

    Refactor password length input handling and remove requirements.txt

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ import random
 import os
 
 data_system = os.getenv("LENGTH_PASSWORD")
+
+
 if data_system is not None:
     length_password = int(data_system)
 else:
-    length_password = int(input("How many characters do you want in your password? : "))
+    while True:
+        length_password = int(input("How many characters do you want in your password? : "))
+        if length_password >= 8 and length_password <= 20:
+            break
+        else:
+            print("Your password must be between 8 and 20 characters, please try again.")
 
 all_characters = string.ascii_letters + string.digits + string.punctuation
 my_password = ""
